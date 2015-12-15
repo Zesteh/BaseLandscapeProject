9ea07145ff444111d8ba388c0ed515dbe1dc93bd
unknown <b015493e@k106-12.student.staffs.ac.uk>
2015-12-15 17:08:51 +0000

    added the changes to the helloworld scene class and header along with the updates to the mainscene

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -1,7 +1,9 @@
 #include "HelloWorldScene.h"
 #include "cocostudio/CocoStudio.h"
 #include "ui/CocosGUI.h"
-
+#include "cocostudio\CocoStudio.h"
+#include "SkiGame.h"
+#include "EndGame.h"
 // Includes required for Pseudo Random Numbers
 #include <cstdlib>
 #include <ctime>
@@ -29,72 +31,40 @@ Scene* HelloWorld::createScene()
 // on "init" you need to initialize your instance
 bool HelloWorld::init()
 {
-    //////////////////////////////
-    // 1. super init first
-    if ( !Layer::init() )
-    {
-        return false;
-    }
+	//////////////////////////////
+	// 1. super init first
+	if (!Layer::init())
+	{
+		return false;
+	}
 
 	// First, lets create our seed for the random number generator
 	srand(time(NULL));
-    
-    auto rootNode = CSLoader::createNode("MainScene.csb");
 
-    addChild(rootNode);
+	auto rootNode = CSLoader::createNode("MainScene.csb");
 
-	// Add the label to give us some feedback on where we have touched
-	labelTouchInfo = Label::createWithSystemFont("Touch or click on the screen", "Arial", 30);
 
-	// Set the label to be in the middle of the screen
-	labelTouchInfo->setPosition(cocos2d::Vec2(Director::getInstance()->getVisibleSize().width / 2, Director::getInstance()->getVisibleSize().height / 2));
+	auto StartButton = rootNode->getChildByName<cocos2d::ui::Button*>("StartButton");
+	auto ExitButton = rootNode->getChildByName<cocos2d::ui::Button*>("ExitButton");
 
-	// Create a custom event listener
-	auto touchListener = EventListenerTouchOneByOne::create();
+	StartButton->addTouchEventListener([&](Ref* sender, cocos2d::ui::Widget::TouchEventType Event)
+	{
+		if (Event == ui::Widget::TouchEventType::ENDED)
+		{
+			auto scene = SkiGame::createScene();
+			Director::getInstance()->replaceScene(scene);
+		}
+	});
 
-	// Assign the event methods to the event listener (known as callbacks)
-	touchListener->onTouchBegan = CC_CALLBACK_2(HelloWorld::onTouchBegan, this);
-	touchListener->onTouchEnded = CC_CALLBACK_2(HelloWorld::onTouchEnded, this);
-	touchListener->onTouchMoved = CC_CALLBACK_2(HelloWorld::onTouchMoved, this);
-	touchListener->onTouchCancelled = CC_CALLBACK_2(HelloWorld::onTouchCancelled, this);
+	ExitButton->addTouchEventListener([&](Ref* sender, cocos2d::ui::Widget::TouchEventType Event)
+	{
+		if (Event == ui::Widget::TouchEventType::ENDED)
+		{
+			CCDirector::sharedDirector()->end();
+		}
+	});
 
-	/* For more information on the eventdispatcher mechanism (and how events in Cocos work in general) go to http://www.cocos2d-x.org/wiki/EventDispatcher_Mechanism */
-	// Add the event listener to the event dispatcher
-	_eventDispatcher->addEventListenerWithSceneGraphPriority(touchListener, this);
-
-	// Finally, lets add the label to the layer (HelloWorld is a layer, NOT a scene)
-	this->addChild(labelTouchInfo);
-
-	
+ 
 
     return true;
-}
-
-bool HelloWorld::onTouchBegan(cocos2d::Touch* touch, cocos2d::Event* event)
-{
-	// Create three random numbers that will be the colour of our text
-	GLubyte red = rand() % 255;
-	GLubyte green = rand() % 255;
-	GLubyte blue = rand() % 255;
-	// Create the colour based on our random numbers
-	cocos2d::Color3B myColour = cocos2d::Color3B(red, green, blue);
-	labelTouchInfo->setPosition(touch->getLocation());
-	labelTouchInfo->setString("You pressed HERE!");
-	labelTouchInfo->setColor(myColour);
-	return true;
-}
-
-void HelloWorld::onTouchEnded(cocos2d::Touch* touch, cocos2d::Event* event)
-{
-	
-}
-
-void HelloWorld::onTouchMoved(cocos2d::Touch* touch, cocos2d::Event* event)
-{
-
-}
-
-void HelloWorld::onTouchCancelled(cocos2d::Touch* touch, cocos2d::Event* event)
-{
-
 }
--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -2,6 +2,7 @@
 #define __HELLOWORLD_SCENE_H__
 
 #include "cocos2d.h"
+#include "ui/CocosGUI.h"
 
 class HelloWorld : public cocos2d::Layer
 {
@@ -12,16 +13,10 @@ public:
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();
 
-	virtual bool onTouchBegan(cocos2d::Touch*, cocos2d::Event*);
-	virtual void onTouchEnded(cocos2d::Touch*, cocos2d::Event*);
-	virtual void onTouchMoved(cocos2d::Touch*, cocos2d::Event*);
-	virtual void onTouchCancelled(cocos2d::Touch*, cocos2d::Event*);
-
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
 
-private:
-	cocos2d::Label* labelTouchInfo;
+
 };
 
-#endif // __HELLOWORLD_SCENE_H__
+#endif
